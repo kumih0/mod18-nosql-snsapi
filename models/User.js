a97bb427b16104fabef3e94e18ae4cd96f97d0f6
KT <132851569+kumih0@users.noreply.github.com>
2023-07-18 21:51:04 -0500

    finished user model schema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,42 @@
 const { Schema, model } = require('mongoose');
+
+//creating the user schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'You need to provide a username!',
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: 'You need to provide an email address!',
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [this] //self-reference user model as array
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total count of friends 
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model('User', userSchema);
+
+module.exports = User;
