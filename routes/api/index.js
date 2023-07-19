cd3ae00f7236b83071d68227d6026e13302e59dc
KT <132851569+kumih0@users.noreply.github.com>
2023-07-18 22:20:33 -0500

    api  index file completed

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thotRoutes');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
